83dfd7ca081eedca321f2938bad45abe72c8fe5f
nobody101212 <adahankachaganovvv@gmail.com>
2026-01-09 14:34:39 +0600

    Project

--- main.py
+++ main.py
@@ -1,36 +1,34 @@
 import flet as ft
+from datetime import datetime
 
 
 def main(page: ft.Page):
     page.title = 'Мое первое приложение'
-    
-    
+
     text_hello = ft.Text('Hello world')
-    
-    def text_name():
-        # print(name_input.value)
+
+    name_input = ft.TextField(label='Введите что-нибудь')
+
+    def text_name(e):
         name = name_input.value.strip()
 
         if name:
+            now = datetime.now()
+            time_str = now.strftime("%Y:%m:%d - %H:%M:%S")
+
             text_hello.color = None
-            text_hello.value = f'Hello {name}'
-            name_input.value = None
+            text_hello.value = f'Hello {name}\n{time_str}'
+            name_input.value = ""
         else:
             text_hello.value = "Введите имя!"
             text_hello.color = ft.Colors.RED
 
-    
-         
+        page.update()
+
     elevated_button = ft.ElevatedButton('send', on_click=text_name)
-    
 
-    name_input = ft.TextField(label='Введите что-нибудь')
-     
-   
 
-    thememode_button = ft.IconButton(icon=ft.Icons.BRIGHTNESS_7) 
-    
-    page.add(text_hello,  name_input, elevated_button, thememode_button)
+    page.add( text_hello,name_input,elevated_button)
 
-    
+
 ft.app(target=main)
